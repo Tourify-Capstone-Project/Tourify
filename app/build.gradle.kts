76b33ebb6e0a09d614102d3e7ddabb8587fa5f56
rachmafadhillah <130447481+rachmafadhillah@users.noreply.github.com>
2024-05-27 00:55:25 +0700

    update navigasi, estimate, and favorite

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.capstone.project.tourify"
-        minSdk = 33
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -45,4 +45,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation("nl.joery.animatedbottombar:library:1.1.0")
+    implementation("com.google.android.material:material:1.9.0")
 }
