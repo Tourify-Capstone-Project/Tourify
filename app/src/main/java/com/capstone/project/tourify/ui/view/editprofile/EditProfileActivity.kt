bb0be1a0b34a7368f2e6ff16002e939295758de8
rachmafadhillah <130447481+rachmafadhillah@users.noreply.github.com>
2024-06-15 10:00:37 +0700

    add photo from camera and gallery

--- app/src/main/java/com/capstone/project/tourify/ui/view/editprofile/EditProfileActivity.kt
+++ app/src/main/java/com/capstone/project/tourify/ui/view/editprofile/EditProfileActivity.kt
@@ -2,18 +2,31 @@ package com.capstone.project.tourify.ui.view.editprofile
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.Manifest
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
 import com.capstone.project.tourify.R
 import com.capstone.project.tourify.databinding.ActivityEditProfileBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.userProfileChangeRequest
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditProfileBinding
     private lateinit var auth: FirebaseAuth
+
+    private var imageUri: Uri? = null // variabel untuk menyimpan URI gambar dari kamera
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +48,136 @@ class EditProfileActivity : AppCompatActivity() {
 
             updateUsernameInFirebase(updatedUsername)
         }
+
+        binding.btnAddPhoto.setOnClickListener {
+            showImagePicker()
+        }
     }
+
+    private fun showImagePicker() {
+        if (checkSelfPermission(android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // Izin kamera belum diberikan, minta izin secara dinamis
+            requestCameraPermission()
+        } else {
+            val options = arrayOf<CharSequence>("Ambil Foto", "Pilih dari Galeri")
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Pilih Aksi")
+            builder.setItems(options) { dialog, which ->
+                when (which) {
+                    0 -> {
+                        // Ambil Foto (Kamera)
+                        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                            takePictureIntent.resolveActivity(packageManager)?.also {
+                                val photoFile: File? = try {
+                                    createImageFile()
+                                } catch (ex: IOException) {
+                                    Log.e(TAG, "Error creating image file: ${ex.message}")
+                                    null
+                                }
+                                photoFile?.also {
+                                    val photoURI: Uri = FileProvider.getUriForFile(
+                                        this,
+                                        "${packageName}.fileprovider",
+                                        it
+                                    )
+                                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                                    imageUri = photoURI
+                                    startActivityForResult(takePictureIntent, TAKE_PHOTO)
+                                }
+                            }
+                        }
+                    }
+
+                    1 -> {
+                        // Pilih dari Galeri
+                        Intent(Intent.ACTION_GET_CONTENT).also { pickPhotoIntent ->
+                            pickPhotoIntent.type = "image/*"
+                            startActivityForResult(
+                                Intent.createChooser(
+                                    pickPhotoIntent,
+                                    "Pilih Gambar"
+                                ), PICK_IMAGE
+                            )
+                        }
+                    }
+                }
+            }
+            builder.show()
+        }
+    }
+
+    private fun requestCameraPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            AlertDialog.Builder(this, R.style.AlertDialogTheme)
+                .setTitle("Izin Diperlukan")
+                .setMessage("Aplikasi ini memerlukan izin kamera untuk mengambil foto.")
+                .setPositiveButton(getString(R.string.izinkan)) { _, _ ->
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.CAMERA),
+                        CAMERA_PERMISSION_CODE
+                    )
+                }
+                .setNegativeButton(getString(R.string.batal)) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create()
+                .show()
+        } else {
+            AlertDialog.Builder(this, R.style.AlertDialogTheme)
+                .setTitle("Izin Diperlukan")
+                .setMessage("Aplikasi ini memerlukan izin kamera untuk mengambil foto.")
+                .setPositiveButton(getString(R.string.izinkan)) { _, _ ->
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.CAMERA),
+                        CAMERA_PERMISSION_CODE
+                    )
+                }
+                .setNegativeButton(getString(R.string.batal)) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .create()
+                .show()
+        }
+    }
+    private fun createImageFile(): File {
+        val timeStamp: String =
+            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir: File? = getExternalFilesDir("Pictures")
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        ).apply {
+            imageUri = FileProvider.getUriForFile(
+                this@EditProfileActivity,
+                "${packageName}.fileprovider",
+                this
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                PICK_IMAGE -> {
+                    val selectedImageUri = data?.data
+                    binding.imgUser.setImageURI(selectedImageUri)
+                    imageUri = selectedImageUri
+                }
+
+                TAKE_PHOTO -> {
+                    imageUri?.let { uri ->
+                        binding.imgUser.setImageURI(uri)
+                    }
+                }
+            }
+        }
+    }
+
 
     private fun updateUsernameInFirebase(updatedUsername: String) {
         val currentUser = auth.currentUser
@@ -49,14 +191,12 @@ class EditProfileActivity : AppCompatActivity() {
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
                         Log.d(TAG, "User profile updated.")
-                        // Kirim kembali hasil ke ProfileFragment
                         val resultIntent = Intent()
                         resultIntent.putExtra("updatedUsername", updatedUsername)
                         setResult(Activity.RESULT_OK, resultIntent)
                         finish()
                     } else {
                         Log.w(TAG, "Failed to update profile", task.exception)
-                        // Handle failure here
                     }
                 }
         }
@@ -77,5 +217,8 @@ class EditProfileActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG = "EditProfileActivity"
+        private const val PICK_IMAGE = 1
+        private const val TAKE_PHOTO = 2
+        private const val CAMERA_PERMISSION_CODE = 101
     }
 }
